435aafef5b1a81cd692f373a09227de1dbc690af
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-11 00:11:17 +0500

    adding plus and minus buton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Number from './Number';
 import './App.css';
+import Minus from './Minus';
+import Add from './Add';
 
 function App() {
   return (
     <div className="App">
       <Number/>
+      <Minus/>
+      <Add/>
     </div>
   );
 }
