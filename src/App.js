90b12f6774efe5da4f6bc7fc4260ef0107e6a39b
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-11 00:20:04 +0500

    craete context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Number from './Number';
 import './App.css';
 import Minus from './Minus';
 import Add from './Add';
 
+const Num = createContext();
+
 function App() {
   return (
     <div className="App">
+    <Num.Provider>
       <Number/>
       <Minus/>
       <Add/>
+      </Num.Provider>  
     </div>
   );
 }
